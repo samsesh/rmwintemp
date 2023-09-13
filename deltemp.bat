c3ee84c4dd213baff875c3f3e6e35f1202264fad
127.0.0.1:443 <admin@samsesh.net>
2023-09-14 01:26:15 +0330

    Update deltemp.bat

--- deltemp.bat
+++ deltemp.bat
@@ -7,6 +7,7 @@ del C:\Users\%username%\AppData\Local\Temp\*.* /s /f /q
 del C:\Windows\Temp\*.* /s /f /q
 start %windir%\system32\cleanmgr.exe /sagerun:1
 start %windir%\system32\cleanmgr.exe /sagerun:65535
+"
 cls
 echo your temp cleaned up
 timeout 03 > nul
@@ -16,4 +17,4 @@ echo https://github.com/samsesh
 timeout 05 > nul
 cls
 timeout 10 > nul
-"
+
