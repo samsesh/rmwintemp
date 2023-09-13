62d032208457d6e0e5b094c09fd78d9a2f5fab91
127.0.0.1:443 <admin@samsesh.net>
2023-09-14 01:25:44 +0330

    Update deltemp.bat

--- deltemp.bat
+++ deltemp.bat
@@ -1,17 +1,19 @@
 @echo off
-echo "delete all temp file on your system"
+echo delete all temp file on your system
 timeout 10
+start /min cmd /c "
 del /q /f /s %TEMP%\*s
-del "C:\Users\%username%\AppData\Local\Temp\*.*" /s /f /q
-del "C:\Windows\Temp\*.*" /s /f /q
-%windir%\system32\cleanmgr.exe /sagerun:1
-%windir%\system32\cleanmgr.exe /sagerun:65535
+del C:\Users\%username%\AppData\Local\Temp\*.* /s /f /q
+del C:\Windows\Temp\*.* /s /f /q
+start %windir%\system32\cleanmgr.exe /sagerun:1
+start %windir%\system32\cleanmgr.exe /sagerun:65535
 cls
 echo your temp cleaned up
-timeout 03 > null
+timeout 03 > nul
 echo Follow me on github
-timeout 02 > null
+timeout 01 > nul
 echo https://github.com/samsesh
-timeout 05 > null
-cls 
-timeout 10 > null 
+timeout 05 > nul
+cls
+timeout 10 > nul
+"
